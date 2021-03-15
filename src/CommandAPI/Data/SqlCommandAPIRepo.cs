320d102a92089d67ada413a5c9f8a8e9e3760386
Salvador Carrillo <Salvador.Carrillo1@partner.magna.com>
2021-03-15 11:49:12 -0600

    applying migrations and implementing get all commands and get by id endpoints

---
+++ src/CommandAPI/Data/SqlCommandAPIRepo.cs
@@ -0,0 +1,46 @@
+using System.Collections.Generic;
+using CommandAPI.Models;
+using System.Linq;
+
+
+namespace CommandAPI.Data
+{
+    public class SqlCommandAPIRepo : ICommandAPIRepo
+    {
+        private readonly CommandContext context;
+
+        public SqlCommandAPIRepo(CommandContext context)
+        {
+            this.context = context;
+        }
+        public void CreateCommand(Command cmd)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void DeleteCommand(Command cmd)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public IEnumerable<Command> GetAllCommands()
+        {
+            return context.CommandItems.ToList();
+        }
+
+        public Command GetCommandById(int id)
+        {
+            return context.CommandItems.FirstOrDefault(p => p.Id == id);
+        }
+
+        public bool SaveChanges()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void UpdateCommand(Command cmd)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
