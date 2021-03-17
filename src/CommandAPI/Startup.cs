c5f6effea727c1725d24d674abcd2efcdce74f68
Salvador Carrillo <Salvador.Carrillo1@partner.magna.com>
2021-03-17 16:47:21 -0600

    secrets implemented

--- src/CommandAPI/Startup.cs
+++ src/CommandAPI/Startup.cs
@@ -10,6 +10,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.EntityFrameworkCore;
+using Npgsql;
 
 
 namespace CommandAPI
@@ -25,13 +26,16 @@ namespace CommandAPI
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            var builder = new NpgsqlConnectionStringBuilder();
+            builder.ConnectionString = Configuration.GetConnectionString("PostgreSqlConnection");
+            builder.Username = Configuration["UserID"];
+            builder.Password = Configuration["Password"];
+
+
             services.AddDbContext<CommandContext>(opt => opt.UseNpgsql
-            (Configuration.GetConnectionString("PostgreSqlConnection")));
+            (builder.ConnectionString));
             //Section 1 add code
             services.AddControllers();
-
-            // services.AddScoped<ICommandAPIRepo, MockCommandAPIRepo>();
-
             services.AddScoped<ICommandAPIRepo, SqlCommandAPIRepo>();
 
         }
